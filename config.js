180705ebe4842a895dadd8434a40039da3171487
wangkai123456 <1205669217@qq.com>
2018-03-06 16:47:00 +0800

    冷柜样式框架完成

---
+++ config.js
@@ -0,0 +1,9 @@
+
+var config = {
+  // rootUrl: 'https://m.forestcoldchain.com' //线上
+  rootUrl: 'https://t.forestcoldchain.com' //测试
+}
+
+config.appUrl = config.rootUrl +'/wafer'
+
+module.exports = config
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,6 @@
+
+const config = require('../config.js')
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -13,7 +16,40 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+// 登录 获取用户信息
+const userLogin = ()=> {
+  wx.login({
+    success : res=> {
+      const rdata = {}; //用户信息以及授权信息
+      // 获取用户信息
+      wx.getUserInfo({
+        success:info=> {
+          console.log(info)
+          rdata.rawData = info.rawData
+          rdata.encryptedData = info.encryptedData
+          rdata.iv = info.iv
+          rdata.signature = info.signature
+        }
+      })
+      // 把用户信息传给后台
+      wx.request({
+        url: config.rootUrl + '/user/wxlogin',
+        data: rdata,
+        success: res => {
+          if (res.statusCode != 200) {
+            wx.showToast({
+              title: '服务器连接失败',
+            })
+          } else {
+            // typeof func == 'function' && func(res.data)
+          }
+        } // request中的success回调结束
+      })
+    }
+  })
+}
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  userLogin: userLogin                                  //登录
 }
